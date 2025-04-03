e1dd531d1580bc19753cd853250df8692f114b63
rogi-rogi <yrkim6883@gmail.com>
2025-04-03 13:58:01 +0900

    프로젝트 최초 생성 및 gitignore 설정

---
+++ src/main/java/com/dockersim/DockerSimApplication.java
@@ -0,0 +1,11 @@
+package com.dockersim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSimApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSimApplication.class, args);
+	}
+}
